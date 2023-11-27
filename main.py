a3529636d474063794256be0e939c6af5c7a2723
ArtyomBobr <artyombobr@gmail.com>
2023-11-27 23:40:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 import os
-import requests
-import sqlalchemy as db
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
-from telegram.ext import (
-    Application,
-    ChatMemberHandler,
-    CommandHandler,
-    ContextTypes,
-    MessageHandler,
-    filters,
-)
-
-
 
 
 def main():
-    print("TEST ENV: " + os.environ.get("CAR_ALERT_BOT_TOKEN"))
+    if os.environ.get("CAR_ALERT_BOT_TOKEN") == "ARTYOMBOBR":
+        print("GOOD")
+    else: 
+        print("ERROR")
 
 
 if __name__ == "__main__":
