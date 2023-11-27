26b109b6d37731175afc8229a71678d8f7d91739
ArtyomBobr <artyombobr@gmail.com>
2023-11-27 03:33:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,20 +97,20 @@ bot = telegram.Bot(token=os.environ.get("CAR_ALERT_BOT_TOKEN"))
 
 
 async def send_message(car_info):
-    print(car_info)
-    session.add(Cars(
-        id=car_info["vin"],
-        description=car_info["description"],
-        url=car_info["url"],
-        image_url=car_info["image_url"]
-    ))
+    # print(car_info)
+    # session.add(Cars(
+    #     id=car_info["vin"],
+    #     description=car_info["description"],
+    #     url=car_info["url"],
+    #     image_url=car_info["image_url"]
+    # ))
     await bot.send_photo(
         chat_id=chat_id,
         caption="<a href='" + car_info["url"] + "'>" + car_info["description"] + "</a>",
         photo=car_info["image_url"],
         parse_mode="HTML"
     )
-    session.commit()
+    # session.commit()
 
 
 def get_sent_cars():
@@ -124,19 +124,18 @@ def get_sent_cars():
 
 async def main():
     new_cars = openlane_cars()
-    sent_cars = get_sent_cars()
+    # sent_cars = get_sent_cars()
 
     messages = list()
     for vin in new_cars:
-        if vin in sent_cars:
-            continue
+        # if vin in sent_cars:
+        #     continue
         messages.append(send_message(new_cars[vin]))
 
     await asyncio.gather(*messages)
 
-    session.close()
+    # session.close()
 
 
 if __name__ == "__main__":
-    print("CAR_ALERT_BOT_TOKEN: " + os.environ.get("CAR_ALERT_BOT_TOKEN"))
     asyncio.run(main())
